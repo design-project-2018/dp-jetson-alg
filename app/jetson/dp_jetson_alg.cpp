76b106b8b15a7729b584e74dceb70ac52e680665
John Wu <john.wu@mail.mcgill.ca>
2018-04-06 03:46:01 +0000

    Added comments

--- app/jetson/dp_jetson_alg.cpp
+++ app/jetson/dp_jetson_alg.cpp
@@ -21,6 +21,17 @@ int main(int argc, char** argv)
         return 0;
     }
 
+    // capture video
+    VideoCapture capture(argv[1]);
+    if(!capture.isOpened())
+    {
+        cout << "cannot read video file" << std::endl;
+        return 0;
+    }
+
+    // to store current frame
+    Mat frame;
+
     // instantiate yolo functions
     Yolo yolo;
     yolo.setConfigFilePath("/home/nvidia/Desktop/darknet/cfg/yolov2.cfg");
@@ -29,51 +40,50 @@ int main(int argc, char** argv)
     yolo.setNameListFile("/home/nvidia/Desktop/darknet/data/coco.names");
     yolo.setThreshold(0.16);
 
-    VideoCapture capture(argv[1]);
-    if(!capture.isOpened())
-    {
-        cout << "cannot read video file" << std::endl;
-        return 0;
-    }
-
-    Mat frame;
+    // instantiate farneback objects
     Mat flow;
     UMat flowUmat, prevgray;
+
+    // repeatedly do computations for each frame
     while(true) {
+
+        // capture frame
         capture >> frame;
         if(frame.empty())
             break;
 
+        // resize frame
         resize(frame, frame, Size(600,400));
 
+        // make copy of frame and turn it into gray for optical flow computation
         Mat img;
         frame.copyTo(img);
-
-        // just make current frame gray
         cvtColor(img, img, COLOR_BGR2GRAY);
 
+        // if a previous frame exists go ahead with computations
         if (prevgray.empty() == false ) {
-            // calculate optical flow
+
+            // perform optical flow
             calcOpticalFlowFarneback(prevgray, img, flowUmat, 0.4, 1, 12, 2, 8, 1.2, 0);
-            // copy Umat container to standard Mat
             flowUmat.copyTo(flow);
 
-            // compute yolo
+            // perform yolo
             vector<DetectedObject> detection;
             yolo.detect(frame, detection);
 
-            // draw optical flow and yolo
-            for(int i = 0; i < detection.size(); i++)
-            {
-                DetectedObject& o = detection[i];
-                rectangle(frame, o.bounding_box, Scalar(255,0,0), 2);
+            // draw results
+            for(int i = 0; i < detection.size(); i++) {
+                DetectedObject &o = detection[i];
+                rectangle(frame, o.bounding_box, Scalar(0, 0, 255), 2);
 
-                const char* class_name = yolo.getNames()[o.object_class];
-
+                // draw object boxes
+                const char *class_name = yolo.getNames()[o.object_class];
                 char str[255];
-                sprintf(str,"%s", class_name);
-                putText(frame, str, Point2f(o.bounding_box.x,o.bounding_box.y), FONT_HERSHEY_SIMPLEX, 0.6, Scalar(0,0,255), 2);
+                sprintf(str, "%s", class_name);
+                putText(frame, str, Point2f(o.bounding_box.x, o.bounding_box.y), FONT_HERSHEY_SIMPLEX, 0.6,
+                        Scalar(0, 0, 255), 2);
 
+                // draw flow vectors
                 for (int y = o.bounding_box.y; y < o.bounding_box.y + o.bounding_box.height; y += 5) {
                     for (int x = o.bounding_box.x; x < o.bounding_box.x + o.bounding_box.width; x += 5) {
                         // get the flow from y, x position * 10 for better visibility
@@ -87,24 +97,7 @@ int main(int argc, char** argv)
                 }
 
             }
-
-//
-//
-//            // By y += 5, x += 5 you can specify the grid
-//            for (int y = 0; y < img.rows; y += 5) {
-//                for (int x = 0; x < img.cols; x += 5) {
-//
-//
-//                    // get the flow from y, x position * 10 for better visibility
-//                    const Point2f flowatxy = flow.at<Point2f>(y, x) * 10;
-//                    // draw line at flow direction
-//                    line(frame, Point(x, y), Point(cvRound(x + flowatxy.x), cvRound(y + flowatxy.y)),
-//                         Scalar(255, 0, 0));
-//                    // draw initial point
-//                    circle(frame, Point(x, y), 1, Scalar(0, 0, 0), -1);
-//                }
-//            }
-
+            // display
             imshow("Demo", frame);
             waitKey(1);
         } else {
