947448ddabfba72531bfba162c0afec3e87f0f0e
John Wu <john.wu@mail.mcgill.ca>
2018-04-08 03:51:58 +0000

    Combined all frames in the same window

--- app/jetson/dp_jetson_alg.cpp
+++ app/jetson/dp_jetson_alg.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/video/tracking.hpp>
 #include <vector>
 #include <stdio.h>
+#include <stdarg.h>
 #include <iostream>
 
 #include <yolo.h>
@@ -15,6 +16,115 @@ using namespace cv;
 using namespace std;
 
 int FRAME_FREQ = 4;
+
+/**
+ * Displays muliple frames in the same window
+ *
+ * @param title
+ * @param nArgs
+ */
+void displayFrames(string title, int nArgs, ...) {
+    int size;
+    int i;
+    int m, n;
+    int x, y;
+
+    // w - Maximum number of images in a row
+    // h - Maximum number of images in a column
+    int w , h;
+
+    // scale - How much we have to resize the image
+    float scale;
+    int max;
+
+    // If the number of arguments is lesser than 0 or greater than 12
+    // return without displaying
+    if(nArgs <= 0) {
+        printf("Number of arguments too small....\n");
+        return;
+    }
+    else if(nArgs > 14) {
+        printf("Number of arguments too large, can only handle maximally 12 images at a time ...\n");
+        return;
+    }
+    // Determine the size of the image,
+    // and the number of rows/cols
+    // from number of arguments
+    else if (nArgs == 1) {
+        w = h = 1;
+        size = 500;
+    }
+    else if (nArgs == 2) {
+        w = 2; h = 1;
+        size = 500;
+    }
+    else if (nArgs == 3 || nArgs == 4) {
+        w = 2; h = 2;
+        size = 500;
+    }
+    else if (nArgs == 5 || nArgs == 6) {
+        w = 3; h = 2;
+        size = 400;
+    }
+    else if (nArgs == 7 || nArgs == 8) {
+        w = 4; h = 2;
+        size = 400;
+    }
+    else {
+        w = 4; h = 3;
+        size = 350;
+    }
+
+    // Create a new 3 channel image
+    Mat DispImage = Mat::zeros(Size(100 + size*w, 60 + size*h), CV_8UC3);
+
+    // Used to get the arguments passed
+    va_list args;
+    va_start(args, nArgs);
+
+    // Loop for nArgs number of arguments
+    for (i = 0, m = 20, n = 20; i < nArgs; i++, m += (20 + size)) {
+        // Get the Pointer to the IplImage
+        Mat img = va_arg(args, Mat);
+
+        // Check whether it is NULL or not
+        // If it is NULL, release the image, and return
+        if(img.empty()) {
+            printf("Invalid arguments");
+            return;
+        }
+
+        // Find the width and height of the image
+        x = img.cols;
+        y = img.rows;
+
+        // Find whether height or width is greater in order to resize the image
+        max = (x > y)? x: y;
+
+        // Find the scaling factor to resize the image
+        scale = (float) ( (float) max / size );
+
+        // Used to Align the images
+        if( i % w == 0 && m!= 20) {
+            m = 20;
+            n+= 20 + size;
+        }
+
+        // Set the image ROI to display the current image
+        // Resize the input image and copy the it to the Single Big Image
+        Rect ROI(m, n, (int)( x/scale ), (int)( y/scale ));
+        Mat temp; resize(img,temp, Size(ROI.width, ROI.height));
+        temp.copyTo(DispImage(ROI));
+    }
+
+    // Create a new window, and show the Single Big Image
+    namedWindow( title, 1 );
+    imshow( title, DispImage);
+    waitKey(1);
+
+    // End the number of arguments
+    va_end(args);
+}
 
 int main(int argc, char** argv)
 {
@@ -36,10 +146,10 @@ int main(int argc, char** argv)
 
     // instantiate yolo functions
     Yolo yolo;
-    yolo.setConfigFilePath("darknet/cfg/yolov2.cfg");
-    yolo.setDataFilePath("darknet/cfg/coco.data");
-    yolo.setWeightFilePath("darknet/yolov2.weights");
-    yolo.setNameListFile("darknet/data/coco.names");
+    yolo.setConfigFilePath("/home/nvidia/Desktop/dp-jetson-alg/darknet/cfg/yolov2.cfg");
+    yolo.setDataFilePath("/home/nvidia/Desktop/dp-jetson-alg/darknet/cfg/coco.data");
+    yolo.setWeightFilePath("/home/nvidia/Desktop/dp-jetson-alg/darknet/yolov2.weights");
+    yolo.setNameListFile("/home/nvidia/Desktop/dp-jetson-alg/darknet/data/coco.names");
     yolo.setThreshold(0.5);
 
     // instantiate farneback objects
@@ -116,11 +226,7 @@ int main(int argc, char** argv)
 
             }
             // display
-            imshow("Original", originalFrame);
-            imshow("YOLO", yoloFrame);
-            imshow("Farneback", flowFrame);
-            imshow("Combined", frame);
-            waitKey(1);
+            displayFrames("YOLO and Optical Flow", 4, originalFrame, yoloFrame, flowFrame, frame);
         } else {
             img.copyTo(prevgray);
         }
