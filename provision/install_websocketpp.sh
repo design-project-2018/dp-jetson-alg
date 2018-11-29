4414207cc5a773c3e927a3eae19a105d0817e43e
John Wu <john.wu@mail.mcgill.ca>
2018-11-29 03:18:00 -0500

    Updated provision script

--- provision/install_websocketpp.sh
+++ provision/install_websocketpp.sh
@@ -2,9 +2,7 @@ echo -----------------------------------
 echo INSTALL WEBSOCKETPP
 echo -----------------------------------
 
-cd ..
-git clone https://github.com/zaphoyd/websocketpp
-cd websocketpp
+cd ../websocketpp
 mkdir build && cd build
 cmake .. && make install
 cd ../../
